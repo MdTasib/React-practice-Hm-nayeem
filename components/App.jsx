aebd3402fcf77aa05ae49ea5329f30c2541a8fe7
Mohammad Tasib <68366378+MdTasib@users.noreply.github.com>
2021-03-12 23:38:08 +0600

    React Core concept
    React core concept

---
+++ components/App.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Timer from './Timer/Index';
+import Styleing from './UseCssStyle';
+import EventHandle from './EventHandle/Index';
+import Inputs from './Input';
+import UnControlForm from './UnControlForm/UnControlForm';
+import ControlForm from './ControlForm/ControlForm';
+import SingUpForm from './SingUpForm/SingUpForm';
+
+class App extends Component {
+
+    render() {
+        return (
+            <div>
+                {/* <Timer /> */}
+
+                {/* <Styleing /> */}
+
+                {/* <EventHandle /> */}
+
+                {/* <Inputs /> */}
+
+                {/* <UnControlForm /> */}
+
+                {/* <ControlForm /> */}
+
+                <SingUpForm />
+
+            </div>
+        )
+    }
+}
+export default App;
